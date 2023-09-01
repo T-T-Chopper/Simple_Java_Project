70b2a1056515a538eb4da44668b8b153a586daaf
T-T-Chopper <106195536+T-T-Chopper@users.noreply.github.com>
2023-09-01 22:13:34 +0300

    Update and rename Soru2.java to Q2.java

--- Q2.java
+++ Q2.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Soru2 {
+public class Q2 {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     System.out.print("Lutfen iki tane tam sayi giriniz: "); // Ilk istenen sekilde % kullanarak yapilan cozum.
