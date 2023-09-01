0b09d2841150fc908f026a8367bc8dc736e0a9cc
T-T-Chopper <106195536+T-T-Chopper@users.noreply.github.com>
2023-09-01 22:13:03 +0300

    Update and rename Soru1.java to Q1.java

--- Q1.java
+++ Q1.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Soru1 {
+public class Q1 {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     System.out.println("Lutfen not bilgilerini giriniz:");
