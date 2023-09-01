026ef976f6fd85381153337ac22fea0d5d65cccf
T-T-Chopper <106195536+T-T-Chopper@users.noreply.github.com>
2023-09-01 22:13:51 +0300

    Update and rename Soru3.java to Q3.java

--- Q3.java
+++ Q3.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Soru3 {
+public class Q3 {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     System.out.print("Lutfen cm degerini giriniz: ");
